7bf92ce54b650be499880fde25ee6c750a22a47c
kamujusrujan <kamujusrujan01@gmail.com>
2020-05-17 20:30:52 +0530

    implemented textfield and button

--- App.js
+++ App.js
@@ -1,19 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+import React, {useState} from 'react';
+import { StyleSheet, Text, View , TextInput , Button , Alert , use} from 'react-native';
+import Constants from 'expo-constants';
+
+
+
+
+
+
+const app = ()=>{
+
+  const [goal,updategoal] = useState('')
+
+  const al = function(){
+   updategoal()
+  }
+
+
+  return(
+    <View  >
+    <View style = {style.container}>
+    <TextInput style = {style.block , {width : '80%'}} placeholder = "enter the task" />  
+    <Button style = {style.block} title = "add" onPress ={al} /> 
     </View>
-  );
+    </View>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+
+
+
+
+const style =  StyleSheet.create({
+  container : {
+    flexDirection: 'row', 
+    borderBottomColor :'blue',
+    borderWidth : 1,
+    marginTop : Constants.statusBarHeight,
+    flexDirection : 'row',
+    justifyContent : 'space-evenly'
+
   },
+
+
+
+  block : {
+    borderWidth : 1,
+    alignItems : 'center'
+  }
+
 });
+
+
+
+
+
+export default app;
